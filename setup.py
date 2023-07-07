0f431f15aef89e5e54078446253c79f37901c006
Keith Valin <kvalin@redhat.com>
2023-07-07 14:31:30 -0400

    Initial structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name="FrameBench",
+    description="A Benchmark for measuring frame timings from multiple cameras simultaneously",
+    version="1.0.0",
+    author="Keith Valin",
+    author_email="kvalin@redhat.com",
+    install_requires = [
+        "opencv-python"
+    ],
+    entry_points = {
+        "console_scripts": [
+            "framebench = framebench.main:main"
+        ]
+    }
+)
