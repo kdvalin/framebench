2ff85542f6973a7214321d46db133de71a91a8c7
Keith Valin <kvalin@redhat.com>
2023-07-14 10:15:55 -0400

    Expand setup.py with relevant info for PyPi packaging

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     version="1.0.0",
     author="Keith Valin",
     author_email="kvalin@redhat.com",
+    license="GPL-3.0",
+    url="https://github.com/kdvalin/framebench",
+    keywords=["camera", "webcam", "framebench", "test", "benchmark", "frame timings"],
     install_requires = [
         "opencv-python",
         "fire",
@@ -18,5 +21,14 @@ setup(
         "console_scripts": [
             "framebench = framebench.main:main"
         ]
-    }
+    },
+    classifiers=[
+        "5 - Production/Stable",
+        "Topic :: Benchmark :: Cameras"
+        "License :: OSI Approved :: GPL3 License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8"
+    ]
 )
