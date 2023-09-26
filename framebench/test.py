d942f8ef5bea58a0bbfe7538905d3961c75d6725
Keith Valin <kvalin@redhat.com>
2023-09-26 12:18:47 -0400

    Add option for selecting which input format to use

--- framebench/test.py
+++ framebench/test.py
@@ -2,10 +2,10 @@ import ffmpeg
 import tempfile
 
 class CameraTest:
-    def __init__(self, device: str, test_time: int = 30):
+    def __init__(self, device: str, test_time: int = 30, input_format="mjpeg"):
         self.out_tmp = tempfile.mkstemp(suffix=".mkv", prefix=f"framebench-{device.replace('/', '_')}")
         self.stream = ffmpeg.overwrite_output(
-            ffmpeg.input(device, t=test_time).output(self.out_tmp[1], codec="copy")
+            ffmpeg.input(device, t=test_time, input_format=input_format).output(self.out_tmp[1], codec="copy")
         )
 
     def run(self):
