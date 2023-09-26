8d4f76ae2ddb4a3aeae6a533e713420b996f2b76
Keith Valin <kvalin@redhat.com>
2023-09-26 12:17:01 -0400

    Update to copy frames over and not encode them

--- framebench/test.py
+++ framebench/test.py
@@ -5,8 +5,8 @@ class CameraTest:
     def __init__(self, device: str, test_time: int = 30):
         self.out_tmp = tempfile.mkstemp(suffix=".mkv", prefix=f"framebench-{device.replace('/', '_')}")
         self.stream = ffmpeg.overwrite_output(
-            ffmpeg.input(device, t=test_time).output(self.out_tmp[1])
+            ffmpeg.input(device, t=test_time).output(self.out_tmp[1], codec="copy")
         )
-    
+
     def run(self):
         self.stream.run()
