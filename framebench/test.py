02f4dc26b637a18e5d099577edd65b8bdd5cb4cf
Keith Valin <kvalin@redhat.com>
2023-09-26 12:35:39 -0400

    Added resolution option to the test

--- framebench/test.py
+++ framebench/test.py
@@ -2,11 +2,15 @@ import ffmpeg
 import tempfile
 
 class CameraTest:
-    def __init__(self, device: str, test_time: int = 30, input_format="mjpeg"):
+    def __init__(self, device: str, test_time: int = 30, input_format="mjpeg", resolution="640x480"):
+        (width, _, height) = resolution.partition('x')
         self.out_tmp = tempfile.mkstemp(suffix=".mkv", prefix=f"framebench-{device.replace('/', '_')}")
         self.stream = ffmpeg.overwrite_output(
-            ffmpeg.input(device, t=test_time, input_format=input_format).output(self.out_tmp[1], codec="copy")
+            ffmpeg
+            .input(device, input_format=input_format, video_size=(width, height))
+            .output(self.out_tmp[1], codec="copy", t=test_time)
         )
 
     def run(self):
+        print(ffmpeg.compile(self.stream))
         self.stream.run()
