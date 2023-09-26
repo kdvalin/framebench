8e172683ac3e80bc0c7e59c7b4e9ae893b51d671
Keith Valin <kvalin@redhat.com>
2023-09-26 12:42:28 -0400

    Added framerate option to the test

--- framebench/test.py
+++ framebench/test.py
@@ -2,12 +2,18 @@ import ffmpeg
 import tempfile
 
 class CameraTest:
-    def __init__(self, device: str, test_time: int = 30, input_format="mjpeg", resolution="640x480"):
+    def __init__(self, device: str, test_time: int = 30, input_format="mjpeg", resolution="640x480", framerate=10):
         (width, _, height) = resolution.partition('x')
         self.out_tmp = tempfile.mkstemp(suffix=".mkv", prefix=f"framebench-{device.replace('/', '_')}")
         self.stream = ffmpeg.overwrite_output(
             ffmpeg
-            .input(device, input_format=input_format, video_size=(width, height))
+            .input(
+                device,
+                r=framerate,
+                framerate=framerate,
+                input_format=input_format,
+                video_size=(width, height)
+            )
             .output(self.out_tmp[1], codec="copy", t=test_time)
         )
 
