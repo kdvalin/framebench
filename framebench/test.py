fdd248b5ad054db5005d2cdd9bcc0b64b54c4fa9
Keith Valin <kvalin@redhat.com>
2023-09-26 12:04:32 -0400

    Added test time to ffmpeg test, also overwrites temp file.

--- framebench/test.py
+++ framebench/test.py
@@ -2,9 +2,11 @@ import ffmpeg
 import tempfile
 
 class CameraTest:
-    def __init__(self, device: str):
+    def __init__(self, device: str, test_time: int = 30):
         self.out_tmp = tempfile.mkstemp(suffix=".mkv", prefix=f"framebench-{device.replace('/', '_')}")
-        self.stream = ffmpeg.input(device).output(self.out_tmp[1])
+        self.stream = ffmpeg.overwrite_output(
+            ffmpeg.input(device, t=test_time).output(self.out_tmp[1])
+        )
     
     def run(self):
         self.stream.run()
